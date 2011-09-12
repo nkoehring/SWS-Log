02590f1e250842df2c6121424df826bfcc047bd1
Norman Köhring <me@nkoehring.de>
2011-09-12 12:08:59 +0200

    tagging

--- js/weblog.js
+++ js/weblog.js
@@ -7,10 +7,34 @@
 
       var setIndex = function() {
         $.each(cntxt.articles, function(i, attrs) {
-          cntxt.$element().append(
+          cntxt.$element().prepend(
             '<article id="'+i+'">'+
             '<a href="#/'+i+'"><h2>'+attrs.title+'</h2></a>'+
             '<div id="'+i+'_content"></div></article>');
+        });
+      }
+
+      var createTagCloud = function() {
+        tags = {};
+        median = 0;
+        total = 0;
+        $.each(cntxt.articles, function(i, attrs) {
+          $.each(attrs.tags, function(j, tag) {
+            total++;
+            if (tag in tags) tags[tag]++;
+            else {
+              median++;
+              tags[tag] = 1;
+            }
+          });
+        });
+        median = total/median;
+
+        $('#tags').text("");
+        $.each(tags, function(tag, n) {
+          elem = $('<li><a href="#tag/'+tag+'">'+tag+'</a></li>');
+          elem.css("font-size", (1+((n-median)/5))+"em");
+          $('#tags').append(elem);
         });
       }
 
@@ -18,6 +42,7 @@
         cntxt.load('/articles.json').then(function(data) {
           cntxt.articles = data;
           setIndex();
+          createTagCloud();
         }).then(callback);
       } else {
         setIndex();
@@ -31,13 +56,19 @@
 
       this.load('/articles/'+a_id).then(function(article) {
         $('[id$=_content]').hide();
+        article = article.replace("\n", " ");
         rendered_article = textile(article);
-        $('#'+a_id+'_content').html(rendered_article).show();
+        $('#'+a_id+'_content').html(rendered_article).slideDown();
         $('body').scrollTop($('#'+a_id).offset().top);
       });
     });
 
+    this.get('#tag/:tag', function(context) {
+      console.log(this.params['tag']);
+    });
+
     this.get('#/', function(context) {
+
     });
 
   });
