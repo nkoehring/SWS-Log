61e7e6a40d6705fcabeec540055afa8bb173d887
Norman Köhring <n@koehr.in>
2013-02-26 03:07:11 +0100

    new-article script now accepts title and tags as command line arguments

--- new.rb
+++ new.rb
@@ -9,8 +9,8 @@ $template = "Please descripe the articles meta information.
 The title can be any one liner and should not be too long. Tags are a space-
 separated word list. Please do not remove the Markers TITLE: and TAGS:.
 
-TITLE: No title set.
-TAGS: untagged
+TITLE: %title%
+TAGS: %tags%
 
 ===== ARTICLE BEGINS HERE =====
 "
@@ -26,14 +26,29 @@ def save_article(key, article)
   File.open("articles/#{key}", "w") {|fp| fp.write(article)}
 end
 
-def new_article
+def new_article(title="No title set.", tags=["untagged"])
+  # "foo bar" => "FooBar" 'cause we don't want spaces
+  tags = tags.collect do |tag|
+    tagwords = tag.split
+    if tagwords.length > 1
+      tagwords.map(&:capitalize).join
+    else
+      tag.downcase
+    end
+  end.uniq.join(' ')
+  template = $template.gsub("%title%", title).gsub("%tags%", tags)
+
   data = Tempfile.open('sws-log-article-') do |fp|
-    fp.write($template)
+    fp.write(template)
     fp.flush
     system "#{ENV['EDITOR']} #{fp.path}"
-    # TODO check for changes?
     fp.rewind
     fp.read
+  end
+
+  if data == template   # no changes made, cancel
+    puts "Empty article: canceled."
+    return
   end
 
   meta_raw, article = data.split('===== ARTICLE BEGINS HERE =====').collect(&:strip)
@@ -46,11 +61,16 @@ def new_article
   end
 
   key = Time.now.to_i.to_s
-  save_article_info(key, meta)
-  save_article(key, article)
+  print "Article key is #{key}: "
+  save_article_info(key, meta) and print "Updated meta data… "
+  save_article(key, article) and print "Saved file… "
   
   # update the rss feed
-  `./feed.rb`
+  `./feed.rb` and puts "Updated RSS feed."
 end
 
-new_article
+if ARGV.length > 1
+  new_article ARGV.shift, ARGV
+else
+  new_article
+end
